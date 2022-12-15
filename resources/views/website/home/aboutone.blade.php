1957cd5619b8a6bfd74f9a47e9f41f09f1e2cfc8
Ynabin01 <nabin.01least@gmail.com>
2022-12-15 17:38:24 +0545

    done

--- resources/views/website/home/aboutone.blade.php
+++ resources/views/website/home/aboutone.blade.php
@@ -1,20 +1,16 @@
-
-
 <section class="about-area ptb--60">
     <div class="container">
         <div class="row">
             <div class="text-center">
                 <div class="heading">
-                    <h1 class="mb--18">{{$about->caption ?? ''}}</h1>
+                    <h1 class="mb--18">{{ $about->caption ?? '' }}</h1>
                     <hr class="delimeter mx-auto mb--22">
                 </div>
-                <p class="mb--25 text-center">{{$about->short_content ?? ''}}
+                <p class="mb--25 text-center">{{ $about->short_content }}
                     <br>
-                    {{$about->long_content ?? ''}}
-                    </p>
-                <p class="mb--25 text-center">
-                    
+                    {{ $about->long_content }}
                 </p>
+
                 <a href="/about-one" class="btn">Read More</a>
             </div>
         </div>
