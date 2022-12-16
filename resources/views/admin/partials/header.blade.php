c478ecdef334347d49c0a9d2ba53977a1c7b5369
Ynabin01 <nabin.01least@gmail.com>
2022-12-16 19:01:18 +0545

    update

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Dashboard-Employmentlink.com</title>
+  <title>Dashboard-SRS OVERSEAS PRIVATE LIMITED</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="csrf-token" content="{{ csrf_token() }}">
